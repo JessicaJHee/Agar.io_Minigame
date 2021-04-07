d201c793ea57ab8ba4fda39f3eb5952d1a810e12
KevinSadigh <79182046+KevinSadigh@users.noreply.github.com>
2021-04-07 17:16:11 -0400

    Updated agario.c

--- agario.c
+++ agario.c
@@ -12,10 +12,19 @@ typedef struct playerBall{
 	int dy; 
 }playerBall; 
 
+/*typdef enum{
+	menu,
+	game,
+}states; */
+
+//states gamestates; 
+
 void clear_screen();
 void drawBall(const playerBall *ball, short int color); 
 void wait_for_vsync(); 
 void plot_pixel(int x, int y, short int line_color); 
+void readKeyboard(unsigned char *clickedKey);
+void redrawPlayer(playerBall *ball, unsigned char var);
 
 int main(void) {
     volatile int * pixel_ctrl_ptr = (int *)0xFF203020;
@@ -29,14 +38,23 @@ int main(void) {
     wait_for_vsync();
 
     pixel_buffer_start = *pixel_ctrl_ptr;
-	clear_screen();
-	drawBall (&pBall,0xFFFF);
+	//clear_screen();
+	//drawBall (&pBall,0x07E0);
 	
     *(pixel_ctrl_ptr + 1) = 0xC8000000;
     pixel_buffer_start = *(pixel_ctrl_ptr + 1); // we draw on the back buffer
 	
-	drawBall (&pBall,0xFFFF); 
-	 
+	unsigned char clickedKey = 0; 
+	
+	//drawBall (&pBall,0x07E0); 
+	while(true){
+		
+		readKeyboard(&clickedKey);
+		redrawPlayer(&pBall, clickedKey);
+		drawBall(&pBall,0x07E0);
+		
+			
+	}
     return 0;
 }
 void drawBall(const playerBall *ball, short int color) {
@@ -69,3 +87,39 @@ void plot_pixel(int x, int y, short int line_color){
     *(short int *)(pixel_buffer_start + (y << 10) + (x << 1)) = line_color;
 }
 
+void readKeyboard(unsigned char *clickedKey){
+	int dataVar; 
+	volatile int * PS2_ptr = (int *) 0xFF200100;
+	dataVar = *PS2_ptr;
+	*clickedKey = dataVar & 0xFF;
+	//loop 
+	while (dataVar & 0x8000) {
+		dataVar = *PS2_ptr;
+	}
+}
+void redrawPlayer(playerBall *ball, unsigned char var){
+	//for arrow key inputs 
+	if (var == 0x75)
+		ball->dy = 3; 
+	else if (var == 0x72)
+		ball->dy = -3;
+	else if (var == 0x74)
+		ball->dx = 3;
+	else if (var == 0x6B)
+		ball->dx = -3;
+	else{
+		ball->dx = 0;
+		ball->dy = 0;
+	}
+	//add the delta 
+	(ball->y) += (ball->dy); 
+	(ball->x) += (ball->dx);
+	//border cases for x 
+	if (ball->x - ball->radius < 0 || ball->x + ball->radius > 320) {
+		ball->x = ball->x - ball->dx;
+		ball->dx = 0;}
+	//border cases for y 
+	if (ball->y - ball->radius < 0 || ball->y + ball->radius > 240) {
+		ball->y = ball->y - ball->dy;
+		ball->dy = 0;}
+}
